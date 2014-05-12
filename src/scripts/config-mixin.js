1c821e11c5957a3e35bb5e6ebbc2d748a6d5d917
Adam Thody <adam@adamthody.com>
2014-05-12 17:02:50 -0400

    Add comments to config mixin for clarity

--- src/scripts/config-mixin.js
+++ src/scripts/config-mixin.js
@@ -1,18 +1,20 @@
 "use strict";
 
 module.exports = {
+
+    // Sets config object on target prior to mounting
     componentWillMount: function () {
         this.config = {};
 
         if (ENV === 'production') {
+            // in this case, grab configuration from a config management API
             var config = this.config;
 
             $.get("http://configapi.example.com/widget-configs/1234", function (result) {
                 config = result[0];
             });
-        } else if (ENV === 'staging') {
-            this.config.apiBaseUrl = "http://staging.api.example.com";
         } else {
+            // in this case, use static configuration values
             this.config.apiBaseUrl = "http://dev.api.example.com";
         }
     }    
