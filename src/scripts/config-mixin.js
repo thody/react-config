57ed42b6187c0a88f8da9bfd6cadc4eecf25f523
Adam Thody <adam@adamthody.com>
2014-05-14 11:33:30 -0400

    Add rationale to config approach

--- src/scripts/config-mixin.js
+++ src/scripts/config-mixin.js
@@ -1,5 +1,18 @@
 "use strict";
 
+/**
+    One possible approach to "config injection" essentially, that will populate
+    a config object on base classes prior to the React mount phase.
+
+    Pros:
+    - hides config retrieval details from base classes
+    - doesn't pollute component signatures by passing props down
+    - only requires base classes that need config to know about config
+
+    Cons:
+    - setting/reading of ENV var feels sloppy
+    - requires knowledge of all ENV permutations (not easily overridable at build/runtime)
+*/
 module.exports = {
 
     // Sets config object on target prior to mounting
